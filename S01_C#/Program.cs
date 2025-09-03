61e0a3736e79e2207350928b7326ea15576de06c
Leos43d <leos43dgames@gmail.com>
2025-09-03 12:07:13 -0600

    Subiendo ejer 1|

---
+++ S01_C#/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace S01_C_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            EJER_2();
+            Console.ReadKey();
+        }
+
+        static void EJER_1()
+        {
+            String nombre, carrera;
+            Console.WriteLine("Su nombre es: ");
+            nombre = Console.ReadLine();
+            Console.WriteLine("Ingrese su carrera: ");
+            carrera = Console.ReadLine();
+            Console.WriteLine($"\nBienvenido al curso de Fund. de Algoritmos,{nombre} de la carrera {carrera}");
+        }
+
+        static void EJER_2()
+        {
+            String nombre;
+            Console.WriteLine("Su nombre es: ");
+            nombre = Console.ReadLine();
+        }
+
+        static void EJER_3()
+        {
+
+        }
+
+        static void EJER_4()
+        {
+
+        }
+
+        static void EJER_5()
+        {
+
+        }
+
+        static void EJER_6()
+        {
+
+        }
+    }
+}
---
+++ SEM01_PYTHON/SEM01_PYTHON.py
@@ -0,0 +1,7 @@
+def ejer1():
+    nombre=input("Ingrese su nombre: ")
+    carrera=input("Ingrese su carrera: ")
+    
+    print(f"\nBienvenido al curso de Fund. de Algoritmos{nombre}, de la carrera {carrera}")
+
+ejer1()
